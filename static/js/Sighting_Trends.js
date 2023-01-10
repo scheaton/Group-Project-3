ad3fdd8df463aa2bc0106737c32a43689086f51e
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 <galluccimia@gmail.com>
2023-01-10 16:43:14 -0500

    updates to sighting_trends

--- static/js/Sighting_Trends.js
+++ static/js/Sighting_Trends.js
@@ -1,1 +1,40 @@
 // last 6 month trends
+
+let names = []
+let abbreviations = []
+
+// For loop to populate arrays
+for (let i = 0; i < usStates.length; i++) {
+  row = usStates[i];
+  names.push(row.name);
+  abbreviations.push(row.abbreviation);
+}
+
+// Trace1 for the State Data
+let trace1 = {
+  x: names,
+  y: sightings,
+  text: names,
+  name: "States",
+  type: "bar"
+};
+
+// Create data array
+let data = [trace1];
+
+// Apply title
+let layout = {
+  title: "Sightings by Month",
+  barmode: "group",
+  // Include margins 
+  margin: {
+    l: 50,
+    r: 50,
+    b: 200,
+    t: 50,
+    pad: 4
+  }
+};
+
+// Render the plot 
+Plotly.newPlot("plot", data, layout);

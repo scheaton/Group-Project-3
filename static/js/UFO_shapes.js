52cf31b4219c79e5db594fd674f20ff3301a9275
Samuel Heaton <itsmesamnit@gmail.com>
2023-01-11 12:04:51 -0500

    formatting improvements to pie chart

--- static/js/UFO_shapes.js
+++ static/js/UFO_shapes.js
@@ -1,10 +1,28 @@
-
-
-
+// Choose how shapes will be organized
 common_shapes_labels = ['Circle', 'Disk', 'Sphere', 'Light', 'Diamond', 'Cigar', 'Rectangle', 'Triangle', 'Oval', 'Formation', 'Changing', 'Other'];
-state_shapes_values = [0,0,0,0,0,0,0,0,0];
-us_shapes_values = [0,0,0,0,0,0,0,0,0];
+all_labels_colors =['#1f77b4',  // muted blue
+                    '#ff7f0e',  // safety orange
+                    '#2ca02c',  // cooked asparagus green
+                    '#d62728',  // brick red
+                    '#9467bd',  // muted purple
+                    '#8c564b',  // chestnut brown
+                    '#e377c2',  // raspberry yogurt pink
+                    '#7f7f7f',  // middle gray
+                    '#bcbd22',  //curry yellow-green
+                    '#17becf',  // blue-teal
+                    '#ffeb3b',  // yellow
+                    '#8bc34a'   // light green
+]
 
+// Make empty arrays the same length as common_shapes_labels
+state_shapes_values = [];
+us_shapes_values = [];
+for (let i = 0; i < common_shapes_labels.length; i++) {
+    state_shapes_values.push(0);
+    us_shapes_values.push(0);
+};
+
+// Function to sort sighting shapes according to common_shapes_labels
 function filterShapes(labels, values, shape) {
     if (labels.includes(shape)) {
         values[labels.indexOf(shape)]++;
@@ -13,18 +31,14 @@ function filterShapes(labels, values, shape) {
     };
 };
 
-
-
 function init() {
-
+    // Dropdown to choose any US State
     let dropDown = d3.select('#selectedStateAbbrev');
-
     for (let i = 0; i < usStates.length; i++) {
         dropDown.append("option").text(usStates[i].abbreviation);
     };
 
-
-
+    // Retrieve data and initialize pie charts for United States and Alabama (alphabetically first state)
     d3.json(sightings_url).then(data => {
         for (let i = 0; i < data.length; i++) {
             shape = data[i].Shape;
@@ -36,37 +50,44 @@ function init() {
         };
 
         // Only take shapes with nonzero sightings
+        state_labels_colors = [];  // neccessary to have label colors match from US chart to State chart 
         state_shapes_labels_nonzero = [];
         state_shapes_values_nonzero = [];
         for (let i = 0; i <= common_shapes_labels.length; i++) {
-            if ((state_shapes_values[i] != 0) && (state_shapes_values[i] != 'NaN')) {
+            if (state_shapes_values[i] != 0) {
                 state_shapes_labels_nonzero.push(common_shapes_labels[i]);
                 state_shapes_values_nonzero.push(state_shapes_values[i]);
+                state_labels_colors.push(all_labels_colors[i]);
             }
         }
-        console.log(state_shapes_values);
-        console.log(state_shapes_values_nonzero);
 
         // Pie Charts
         let stateData = [{
             values: state_shapes_values_nonzero, // values
             labels: state_shapes_labels_nonzero, // shape names
             type: 'pie',
-            automargin: true
+            automargin: true,
+            marker: {
+                colors: state_labels_colors
+              },
+            sort: false // Make labels match from US chart to State chart 
         }];
 
         let usData = [{
             values: us_shapes_values, // values
             labels: common_shapes_labels, // shape names
             type: 'pie',
-            automargin: true
+            automargin: true,
+            marker: {
+                colors: all_labels_colors
+              },
+            sort: false // Make labels match from US chart to State chart 
         }];
 
         let pieLayout = {
         autosize: true,
         margin: {"t": 0, "b": 0, "l": 0, "r": 0},
         paper_bgcolor: 'transparent',
-        // plot_bgcolor: 'transparent',
         legend: {x: 0}
         };
 
@@ -74,7 +95,6 @@ function init() {
         
         Plotly.newPlot('statePieChart', stateData, pieLayout, config); 
         Plotly.newPlot('usPieChart', usData, pieLayout, config);
-
 
     });
 
@@ -102,23 +122,25 @@ function optionChanged() {
         };
 
         // Clean up shapes with 0 sightings
+        // Only take shapes with nonzero sightings
+        state_labels_colors = [];  // neccessary to have label colors match from US chart to State chart 
         state_shapes_labels_nonzero = [];
         state_shapes_values_nonzero = [];
-        for (let i = 0; i < common_shapes_labels.length; i++) {
+        for (let i = 0; i <= common_shapes_labels.length; i++) {
             if (state_shapes_values[i] != 0) {
                 state_shapes_labels_nonzero.push(common_shapes_labels[i]);
                 state_shapes_values_nonzero.push(state_shapes_values[i]);
+                state_labels_colors.push(all_labels_colors[i]);
             }
         }
 
         let stateData = {
             values: [state_shapes_values_nonzero],
-            labels: [state_shapes_labels_nonzero],
+            labels: [state_shapes_labels_nonzero]
         }
 
         Plotly.restyle('statePieChart', stateData, [0]);
     });
-
 
 };
 
